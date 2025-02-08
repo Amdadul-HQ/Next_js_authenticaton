d4ef967abe26acafbc0ce42d092383115768e670
Amdadul-HQ <rimonamdadul301@gmail.com>
2025-02-08 19:36:27 +0600

    add: login and log out function

--- src/app/(withDashboard)/dashboard/page.tsx
+++ src/app/(withDashboard)/dashboard/page.tsx
@@ -12,7 +12,7 @@ const DashboardPage = async () => {
         <h1 className="text-4xl text-center mt-10">Welcome To {session?.user?.name}</h1>
         <p>Log in user email {session?.user?.email}</p>
         <div>
-          <Image width={100} height={100} className="mx-auto rounded-full" src={session?.user?.image}/>
+          <Image alt={session?.user?.name || 'profile'} width={100} height={100} className="mx-auto rounded-full" src={session?.user?.image}/>
         </div>
         </>)
       }
